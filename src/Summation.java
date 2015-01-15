d29b0a2ce4fcbc9463a6d1635cfcd7788273c00b
rizvi <rizvi@bjitgroup.com>
2015-01-15 15:53:32 +0600

    "src/Summation.java is added"

---
+++ src/Summation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+Class Summation{
+	public static void main(String args[]){
+     		int a = 3;
+     		int b = 4;
+		int sum = 0;
+		sum = a+b;
+		System.out.println("Summation of a and b is: "+sum);
+	}
+}
